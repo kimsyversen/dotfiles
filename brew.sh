441bc399831ba9c6c68b55c238c86bdec4f43b70
Kim Syversen <kim.syversen@gmail.com>
2018-06-07 15:19:23 +0200

    Misc improvements

--- brew.sh
+++ brew.sh
@@ -1,22 +1,37 @@
 #!/usr/bin/env bash
 
+# Install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
 # Configure Git
 echo -e "Time to configure some details for Git\n"
 read -p "Enter name # " name
 read -p "Enter email # "  email
 
-# Install xcode command line tools
-#xcode-select –install
-
-# Install homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 #Update homebrew
 brew doctor && brew update
 
-# Install gnu tools
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
-brew install wget
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+
+
+# Install more recent versions of some macOS tools.
+brew install wget --with-iri
+brew install grep
 
 # Install some software
 brew cask install sublime-text sequel-pro spectacle dropbox viscosity bash-completion google-chrome alfred firefox git flux iterm2 spotify vlc the-unarchiver
@@ -32,9 +47,5 @@ brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch 
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
 
-# Setup git
-git config --global user.email "$email"
-git config --global user.name "$name"
-git config --global push.default simple
-
-brew install homebrew/completions/brew-cask-completion
+# Remove outdated versions from the cellar.
+brew cleanup
