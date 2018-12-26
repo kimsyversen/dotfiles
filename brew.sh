f71329dbfc45851b1120f4692dd5b6c44c0c4327
Kim Syversen <kim.syversen@gmail.com>
2018-12-26 11:36:48 +0100

    Added new packages

--- brew.sh
+++ brew.sh
@@ -2,11 +2,6 @@
 
 # Install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Make sure we’re using the latest Homebrew.
-brew update
-
-# Upgrade any already-installed formulae.
 brew upgrade
 
 # Configure Git
@@ -14,13 +9,10 @@ echo -e "Time to configure some details for Git\n"
 read -p "Enter name # " name
 read -p "Enter email # "  email
 
-
-#Update homebrew
-brew doctor && brew update
-
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
@@ -28,24 +20,24 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
 
 # Install more recent versions of some macOS tools.
 brew install wget --with-iri
 brew install grep
 
-# Install some software
-brew cask install sublime-text sequel-pro spectacle dropbox viscosity bash-completion google-chrome alfred firefox git flux iterm2 spotify vlc the-unarchiver
-
-# Install GNU core utilities (those that come with OS X are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+# Install software
+brew cask install virtualbox virtualbox-extension-pack atom \
+sequel-pro spectacle dropbox viscosity bash-completion google-chrome \
+alfred firefox git flux iterm2 spotify vlc the-unarchiver
 
 # Quicklook plugins
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package && qlmanage -r
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json \
+qlprettypatch quicklook-csv betterzipql webpquicklook \
+suspicious-package
 
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+qlmanage -r
 
 # Remove outdated versions from the cellar.
 brew cleanup
