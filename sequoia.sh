ff0ad60f724097e150cca0c434e72158f798ba37
Kim Syversen <kim.syversen@gmail.com>
2024-11-30 16:04:07 +0100

    Create sequoia.sh

---
+++ sequoia.sh
@@ -0,0 +1,3 @@
+# Disable autocorrect and automatic text captialization
+defaults write NSGlobalDomain "NSAutomaticSpellingCorrectionEnabled" -bool FALSE
+defaults write NSGlobalDomain "WebAutomaticSpellingCorrectionEnabled" -bool FALSE
